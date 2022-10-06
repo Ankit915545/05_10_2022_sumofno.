aec215dfbecbaeb0eaad369faa9895f6233b97c7
Ankit915545 <ankiotkumarsharma1996@gmail.com>
2022-10-06 23:01:42 +0530

    simple commit

---
+++ sum.js
@@ -0,0 +1,12 @@
+function sumtriple (a,b){
+    if (a==b){
+        return (3*(a+b));
+    }
+    else{
+        return(a+b);
+    }
+    }
+    console.log(sumtriple(16,20));
+    console.log(sumtriple(10,20)); 
+    console.log(sumtriple(10,10));
+
